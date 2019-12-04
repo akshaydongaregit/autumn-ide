6cebc656d52c8782d2f796d6c978e73a6a0830fd
akshaydongaregit <akshaydongare1797@gmail.com>
2019-12-05 02:31:01 +0530

    home design completed

--- src/resources/js/home.js
+++ src/resources/js/home.js
@@ -6,3 +6,21 @@ $$('.explorer .node label').forEach( el => {
             el.parentElement.classList.toggle("fold");
         });
     });
+
+    var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        lineNumbers: true,
+        styleActiveLine: true,
+        matchBrackets: true
+    });
+
+    let save = () =>{
+        let data = {
+            file : 'first.js' ,
+            content : $('#code').value
+        }
+        axios.post('/save',data).then((res)=>{
+            console.log('res:'+JSON.stringify(res));
+        }).catch((err)=>{
+            console.log('err :'+err);
+        });
+    }
