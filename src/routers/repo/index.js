4fc02162ce3ab4857d787caa6f07e8480e23b326
vishwajeet <vishwajitdesai.940@gmail.com>
2019-12-18 08:12:05 +0530

    create project added

--- src/routers/repo/index.js
+++ src/routers/repo/index.js
@@ -9,14 +9,26 @@ router.post('/save',(req,res)=>{
 router.post('/saveFile',(req,res)=>{
     let data = req.body;
     console.log('router '+data);
-
     res.send(repo.save(data));
+});
+router.post('/newFile',(req,res)=>{
+    let data = req.body;
+    let username = req.session.login;
+    data.name = username+'/'+data.name;
+    console.log('router '+data);
+    res.send(repo.create(data));
+});
+router.post('/deleteFile',(req,res)=>{
+    let data = req.body;
+    let username = req.session.login;
+    data.name = username+'/'+data.name;
+    console.log('router '+data);
+    res.send(repo.delete(data));
 });
 
 router.post('/repostree',(req,res)=>{
     let data = req.body;
     let login = req.session.login;
-
     res.send(repo.dirTreeByLogin(login));
 });
 
@@ -24,7 +36,6 @@ router.post('/repostree',(req,res)=>{
 router.post('/get',(req,res)=>{
     let data = req.body;
     let login = req.session.login;
-
     res.sendFile(global.root+'/repos/' + data.filename);
 });
 
